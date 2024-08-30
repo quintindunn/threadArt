1c99d12a9bd748ab28a41434c0a39ccae2a0702f
Quintin Dunn <93884113+quintindunn@users.noreply.github.com>
2024-08-29 23:42:20 -0400

    Working port

--- threadArt/thread_art.py
+++ threadArt/thread_art.py
@@ -1,166 +1,127 @@
-import os
-import sys
-
-from PIL import Image, ImageOps, ImageDraw
 import numpy as np
+import cv2
+import math
+import time
 
-from skimage.draw import line
-
-try:
-    from .visualizer import StringVisualizer
-except ImportError:
-    from visualizer import StringVisualizer
-
-
-import logging
-
-logger = logging.getLogger("threadArt")
+"""
+CREDIT: https://github.com/halfmonty/StringArtGenerator/blob/master/main.go
+"""
 
 
-def _preprocess_image(img: str | os.PathLike | Image.Image, size: tuple[int, int]) -> Image.Image:
-    """
-    Converts image to grayscale, crops image to a circle. Makes it compatible with the string art algorithm.
-    :param img: Filepath or an Image object.
-    :param size: The size of the output.
-    :return: Image
-    """
+# Constants
+PINS = 300
+MIN_DISTANCE = 30
+MAX_LINES = 4000
+LINE_WEIGHT = 8
+IMG_SIZE = 500
 
-    if isinstance(img, (str, os.PathLike)):
-        img = Image.open(img)
-
-    img = img.convert("L").resize(size)
-    mask = Image.new('L', size, 0)
-    draw = ImageDraw.Draw(mask)
-    draw.ellipse((0, 0) + size, fill=255)
-    mask = mask.resize(img.size, Image.Resampling.LANCZOS)
-    img.putalpha(mask)
-
-    output = ImageOps.fit(img, mask.size, centering=(0.5, 0.5))
-    output.putalpha(mask)
-    logger.debug("Converted and cropped image to a grayscale circle.")
-    return output
+# Globals
+Pin_coords = []
+SourceImage = []
+Line_cache_y = []
+Line_cache_x = []
 
 
-def process_image(im: Image.Image, board_width: int, pixel_width: int, nail_count: int, max_strings: int,
-                  nails_skip: int, visualize: bool = False, progress: int = 200) -> tuple[list[int], Image.Image]:
-    """
-    Generates the string art representation of the image.
-    :param im: Pillow Image to convert to string art.
-    :param board_width: The width of the board.
-    :param pixel_width: The line width.
-    :param nail_count: Number of points around the board to lock onto.
-    :param max_strings: The number of strings to draw.
-    :param nails_skip: How many neighbors to skip when finding the next nail.
-    :param visualize: Whether to show the visualizer.
-    :param progress: Whether to have progress reports on, (Shown on logger level logging.INFO).
-    :return:
-    """
-    pixels = int(board_width / pixel_width)
-    size = (pixels + 1, pixels + 1)
+def main():
+    global SourceImage
+    SourceImage = import_picture_and_get_pixel_array("../dog.jpg")
+    print("Hello, world.")
 
-    visualizer = None
+    start_time = time.time()
+    calculate_pin_coords()
+    precalculate_all_potential_lines()
+    calculate_lines()
+    end_time = time.time()
+    diff = end_time - start_time
+    print(f"precalculateAllPotentialLines Taken: {diff:.6f} seconds")
 
-    if visualize:
-        visualizer = StringVisualizer(nail_count=nail_count, diameter=1024*2)
-        visualizer.generate_nails(live_delay=10)
-
-    base = Image.new('L', size, color=255)
-
-    ref = _preprocess_image(im, size)
-
-    angles = np.linspace(0, 2 * np.pi, nail_count)  # angles to the dots
-    center_x, center_y = (board_width / 2 / pixel_width, board_width / 2 / pixel_width)  # center of circle
-
-    xs = center_x + board_width * 0.5 * np.cos(angles) / pixel_width
-    ys = center_y + board_width * 0.5 * np.sin(angles) / pixel_width
-
-    nails = list(map(lambda x, y: (int(x), int(y)), xs, ys))
-
-    current_nail = 0
-    ref_arr = np.transpose(np.array(ref)[:, :, 0])
-
-    sequence = []
-    for i in range(max_strings):
-        best_line = None
-        new_nail = None
-        min_avg_value = 10**20
-        for n in range(current_nail + 1 + nails_skip, current_nail + len(nails) - nails_skip):
-            n = n % nail_count
-            new_line = line(nails[current_nail][0], nails[current_nail][1], nails[n][0], nails[n][1])
-            num_pts = len(new_line[0])
-
-            tmp_value = np.sum(ref_arr[new_line])
-
-            if tmp_value / num_pts < min_avg_value:
-                best_line = new_line
-                new_nail = n
-                min_avg_value = tmp_value / num_pts
-
-        ref_arr[best_line] = 255
-
-        new_line = ImageDraw.Draw(base)
-        new_line.line((nails[current_nail][0], nails[current_nail][1], nails[new_nail][0], nails[new_nail][1]),
-                      fill=0)
-
-        if visualize and visualizer is not None:
-            visualizer.add_line(current_nail, new_nail)
-            visualizer.update(1)
-
-        seq_len = len(sequence)
-        if new_nail:
-            logger.debug(f"String #{seq_len+1} completed {current_nail} -> {new_nail}")
-            sequence.append(new_nail)
-
-        if progress != -1 and seq_len % progress == 0 and seq_len != nail_count:
-            logger.info(f"Processing: {seq_len/max_strings*100:.2f}%")
-
-        current_nail = new_nail
-
-    if progress != -1:
-        logger.info("Processing: 100%")
-
-    return sequence, base
+    print("End")
 
 
-def _main():
-    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-
-    image_path = "../horse.jpg"
-    width = 4000
-    pixel_size = 1
-    number_of_nails = 288
-    max_strings = 4000
-    nail_skip = 20
-
-    result_file = "../results.txt"
-    result_img = "result.png"
-
-    progress_reports = 200  # After how many lines to print a progress report, 0 to never.
-    use_visualizer = False
-
-    settings = {
-        "Width": width,
-        "Pixel Size": pixel_size,
-        "Number of Nails": number_of_nails,
-        "Max Strings": max_strings,
-        "Nail Skip": nail_skip
-    }
-
-    for k, v in settings.items():
-        print(f"{k}: {v}")
-
-    im = Image.open(image_path)
-    seq, im = process_image(im=im, board_width=width, pixel_width=pixel_size, nail_count=number_of_nails,
-                            max_strings=max_strings, nails_skip=nail_skip, visualize=use_visualizer,
-                            progress=progress_reports)
-
-    with open(result_file, 'w') as f:
-        f.write(','.join(map(str, seq)))
-    im.save(result_img)
-
-    if progress_reports != -1:
-        print(f"Saved result image as: \"{result_img}\", Sequence saved as \"{result_file}\"!")
+def import_picture_and_get_pixel_array(image_path):
+    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
+    return img.flatten().astype(np.float64)
 
 
-if __name__ == '__main__':
-    _main()
+def calculate_pin_coords():
+    global Pin_coords
+    center = IMG_SIZE / 2
+    radius = IMG_SIZE / 2 - 1
+
+    Pin_coords = [
+        (math.floor(center + radius * math.cos(2 * math.pi * i / PINS)),
+         math.floor(center + radius * math.sin(2 * math.pi * i / PINS)))
+        for i in range(PINS)
+    ]
+
+
+def precalculate_all_potential_lines():
+    global Line_cache_y, Line_cache_x
+    Line_cache_y = [[] for _ in range(PINS * PINS)]
+    Line_cache_x = [[] for _ in range(PINS * PINS)]
+
+    for i in range(PINS):
+        for j in range(i + MIN_DISTANCE, PINS):
+            x0, y0 = Pin_coords[i]
+            x1, y1 = Pin_coords[j]
+
+            d = math.floor(math.sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2))
+            xs = np.round(np.linspace(x0, x1, int(d))).astype(int)
+            ys = np.round(np.linspace(y0, y1, int(d))).astype(int)
+
+            Line_cache_y[j * PINS + i] = ys
+            Line_cache_y[i * PINS + j] = ys
+            Line_cache_x[j * PINS + i] = xs
+            Line_cache_x[i * PINS + j] = xs
+
+
+def calculate_lines():
+    print("Drawing Lines....")
+    error = 255 - SourceImage
+
+    line_sequence = []
+    current_pin = 0
+    last_pins = [-1] * 20
+    max_err = 0
+
+    for _ in range(MAX_LINES):
+        best_pin = -1
+        line_err = 0
+        max_err = 0
+        index = 0
+
+        for offset in range(MIN_DISTANCE, PINS - MIN_DISTANCE):
+            test_pin = (current_pin + offset) % PINS
+            if test_pin in last_pins:
+                continue
+            else:
+                inner_index = test_pin * PINS + current_pin
+
+                line_err = get_line_err(error, Line_cache_y[inner_index], Line_cache_x[inner_index])
+                if line_err > max_err:
+                    max_err = line_err
+                    best_pin = test_pin
+                    index = inner_index
+
+        line_sequence.append(best_pin)
+
+        for i in range(len(Line_cache_y[index])):
+            v = int(Line_cache_y[index][i] * IMG_SIZE + Line_cache_x[index][i])
+            error[v] -= LINE_WEIGHT
+
+        last_pins = last_pins[1:] + [best_pin]
+        current_pin = best_pin
+
+    print(line_sequence)
+
+
+def get_line_err(err, coords1, coords2):
+    sum_err = 0
+    for i in range(len(coords1)):
+        sum_err += err[int(coords1[i] * IMG_SIZE + coords2[i])]
+    return sum_err
+
+
+if __name__ == "__main__":
+    main()
