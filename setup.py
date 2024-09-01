17146c0c3e910a7fef72495291072735c5eabb7d
Quintin Dunn <93884113+quintindunn@users.noreply.github.com>
2024-09-01 01:01:05 -0400

    Bumped to version 1.2.0

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "1.1.3"
+VERSION = "1.2.0"
 DESCRIPTION = "A python module for generating thread art sequences."
 
 with open("README.md", 'r') as f:
